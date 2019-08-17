afd0377ac967989b98883cf689e5128ac322752a
CloverFeng <sunkesi02@gmail.com>
2019-08-17 21:12:02 +0800

    Create Arrays Module

---
+++ src/app/arrays/arrays.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArraysRoutingModule } from './arrays-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ArraysRoutingModule
+  ]
+})
+export class ArraysModule { }
